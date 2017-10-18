71cc710fbd8c78fa928532d926ab7d5e5c9012ef
Hanzone <GitHug@163.com>
2017-10-18 16:37:17 +0800

    add some tiny useless stuff

--- src/Test.java
+++ src/Test.java
@@ -3,8 +3,20 @@
  */
 public class Test {
     public static void main(String args[]) {
+
         System.out.println("Hell Yeah!");
         System.out.println("Hell Yeah!");
         System.out.println("Hell Yeah!");
+
+        testMethod();
+    }
+
+    /**
+     * do testing thing
+     */
+    private static void testMethod() {
+
+        System.out.print("ooooooopps");
+
     }
 }
